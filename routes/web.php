d85f5b64473e65f72cb4a55b11e6e7368c7fce56
Nima <alireza.khazforoosh@adventistas.org>
2024-02-16 10:09:09 -0300

    returning data directly from routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ! we can directly return datas:
+Route::get('/directreturn', function () {
+    return response("<h1>Hello, this is a direct returning!</h1>", 200)->header('Hello', 'This is a header that I created!');
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings');
 });
