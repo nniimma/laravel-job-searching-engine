72880ecdbe3bd63561b65eb39e078b3e81bd2976
Nima <alireza.khazforoosh@adventistas.org>
2024-02-19 10:48:22 -0300

    getting rid of fillable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // ! if we want to get rid of fillable we can do this:
+        // ! it allows mass assignment
+        // todo: Model::unguard();
     }
 }
