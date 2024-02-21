676275a4b9a3c79c3883bf295019b73759093009
Nima <alireza.khazforoosh@adventistas.org>
2024-02-21 11:22:54 -0300

    @auth() and {{ auth()->user() }}

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -30,15 +30,29 @@
         <a href="{{ route('listings.index') }}"><img class="w-24" src="{{ URL::asset('images/logo.png') }}"
                 alt="" class="logo" /></a>
         <ul class="flex space-x-6 mr-6 text-lg">
-            <li>
-                <a href="{{ route('register.create') }}" class="hover:text-laravel"><i
-                        class="fa-solid fa-user-plus"></i> Register</a>
-            </li>
-            <li>
-                <a href="{{ route('login') }}" class="hover:text-laravel"><i
-                        class="fa-solid fa-arrow-right-to-bracket"></i>
-                    Login</a>
-            </li>
+            @auth
+                <li>
+                    <span class="font-bold uppercase">
+                        Welcome {{ auth()->user()->name }}
+                    </span>
+                </li>
+                <li>
+                    <a href="{{ route('listings.manage') }}" class="hover:text-laravel">
+                        <i class="fa-solid fa-gear"></i>
+                        Manage Listings
+                    </a>
+                </li>
+            @else
+                <li>
+                    <a href="{{ route('register.create') }}" class="hover:text-laravel"><i
+                            class="fa-solid fa-user-plus"></i> Register</a>
+                </li>
+                <li>
+                    <a href="{{ route('login') }}" class="hover:text-laravel"><i
+                            class="fa-solid fa-arrow-right-to-bracket"></i>
+                        Login</a>
+                </li>
+            @endauth
         </ul>
     </nav>
     <main>
